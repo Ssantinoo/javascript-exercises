02b810ddbef90ae0040383f76a2050a1149c3db7
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-08-25 17:36:58 +0000

    Repeat string n times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    
+    if (num < 0) {
+        return ("ERROR");
+    }
 
+    while (num > 0) {
+        repeatedString += string;
+        num--;
+    }
+
+    return repeatedString;
 };
+
+repeatString("hello", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
