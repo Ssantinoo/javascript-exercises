ca46a6bc375272ace02cf52192de48428f8a7372
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-08-25 17:49:00 +0000

    Reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = string.split("").reverse().join("");
 
+    return reversedString;
 };
+
+reverseString("como estas");
 
 // Do not edit below this line
 module.exports = reverseString;
