9868b3efc63acc095c22eebcae0d8bdf04b822dd
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-08-25 18:40:36 +0000

    Sum a range of values

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+sumAll(80, 20);
 
 // Do not edit below this line
 module.exports = sumAll;
