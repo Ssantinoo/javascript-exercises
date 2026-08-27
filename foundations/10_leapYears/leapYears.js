12f1817213de32bb58c4d40b0ebe99ce967274cd
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-08-27 16:00:01 +0000

    Complete leap years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 };
+
+leapYears(2000);
+leapYears(1985);
 
 // Do not edit below this line
 module.exports = leapYears;
