65edbf2e352e5c133aa527b778cfead92f6ef31b
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-09-12 19:50:16 +0000

    Fibonacci complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const index = Number(num);
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
 
+    let result = [1, 1];
+
+    if (index <= 2) {
+        return result[index - 1];
+    }
+
+    for (let i = 2; i < index; i++) {
+        let last = result[result.length - 1];
+        let previous = result[result.length - 2];
+        result.push(last + previous);
+    }
+
+    return result[result.length - 1];
 };
 
 // Do not edit below this line
